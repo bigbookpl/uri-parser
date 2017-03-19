c923d94788ea668fa7453a86a1707388e05b2e5d
bigbookpl <bigbook.pl@gmail.com>
2017-03-19 23:15:01 +0100

    Refactored SchemaResolver

--- src/Bigbookpl/UriParser/SchemeResolver.php
+++ src/Bigbookpl/UriParser/SchemeResolver.php
@@ -2,71 +2,64 @@
 
 namespace Bigbookpl\UriParser;
 
-use Bigbookpl\UriParser\Parser\Strategy\Generic;
+use Bigbookpl\UriParser\Parser\Strategy as Parser;
 use Bigbookpl\UriParser\Validator\Email;
-use Bigbookpl\UriParser\Validator\Strategy\Validator;
+use Bigbookpl\UriParser\Validator\Strategy as Validator;
 use Bigbookpl\UriParser\Validator\ValidationException;
 
 class SchemeResolver
 {
     private $uri;
     private $validators;
+    private $parsers;
     private $pattern = '/^([[:alpha:]]+[[:alnum:]\+-\.]*):(\/{1,2})?/';
+    private $schemeName;
 
     /**
      * SchemeResolver constructor.
+     *
      * @param string $uri
      */
     public function __construct($uri)
     {
         $this->uri = $uri;
+        $this->schemeName = $this->getSchemeName($this->uri);
     }
 
-    /**
-     * @param Validator $validator
-     */
-    public function addCustomValidator(Validator $validator)
+    public function addCustomValidator(Validator\Validator $validator)
     {
         $this->validators[$validator->getScheme()] = $validator;
     }
 
-    /**
-     * @return Validator
-     */
-    public function resolveValidator(): ?Validator
+    public function resolveValidator(): Validator\Validator
     {
-        $schemeName = $this->getSchemeName($this->uri);
-
-        if (array_key_exists($schemeName,$this->validators)){
-            return $this->validators[$schemeName];
+        if (array_key_exists($this->schemeName, $this->validators)) {
+            return $this->validators[$this->schemeName];
         } else {
             return $this->getGenericValidator();
         }
     }
 
-    /**
-     * @return Validator
-     */
-    public function resolveParser(): Validator
+    public function resolveParser(): Parser\Parser
     {
-
+        if (array_key_exists($this->schemeName, $this->validators)) {
+            return $this->parsers[$this->schemeName];
+        } else {
+            return $this->getGenericParser();
+        }
     }
 
-    private function getGenericParser(){
-        //return new Generic();
+    private function getGenericParser(): Parser\Parser
+    {
+        return new Parser\Generic();
     }
 
-    private function getGenericValidator(){
-        //return new Generic();
+    private function getGenericValidator(): Validator\Validator
+    {
+        return new Validator\Generic();
     }
 
-    /**
-     * Return scheme
-     *
-     * @return string schema
-     * @throws \Exception
-     */
-    private function getSchemeName($uri)
+    private function getSchemeName(string $uri): string
     {
         if (0 == preg_match($this->pattern, $uri, $matches)) {
             throw new ValidationException("Scheme not found");
